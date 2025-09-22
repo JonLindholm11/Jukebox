62c0bf476528aa3104b093b808a650cc00a428e5
jonlindholm11 <jonlindholm11@gmail.com>
2025-09-21 21:20:44 -0400

    querys and routes

--- db/playlist_tracks.js
+++ db/playlist_tracks.js
@@ -3,22 +3,83 @@ const router = express.Router();
 export default router;
 import {
     createPlaylist,
-    createTrack,
-    createPlaylist_track
+    getTracks,
+    createPlaylist_Track,
+    getTracksId,
+    getPlaylists,
+    getTracksByPlaylistId
 } from "./query.js"
 
 router
     .route("/tracks")
     .get(async (req, res) => {
         try {
-        const response = await createTrack()
+        const response = await getTracks()
         res.status(200).send(response)
         } catch (error) {
             res.status(400).send(error)
         }
     })
+
+router
+    .route("/playlists")
+    .get(async (req, res) => {
+        try {
+        const response = await getPlaylists()
+        res.status(200).send(response)
+        } catch (error) {
+            res.status(400).send(error)
+        }
+    }).post(async (req, res) => {
+        try {
+            const response = await createPlaylist(req.body)
+            res.status(200).send(response)
+        } catch (error) {
+            res.status(400).send(error)
+        }
+    })
+
 router
     .route("/tracks/:id")
     .get(async (req, res) => {
-        
+        try {
+        const response = await getTracksId(req.params.id)
+        res.status(200).send(response)
+        } catch (error) {
+            res.status(400).send(error)
+        }
+    })
+
+router
+    .route("/playlists/:id")
+    .get(async (req, res) => {
+        try {
+        const response = await getTracksId(req.params.id)
+        res.status(200).send(response)
+        } catch (error) {
+            res.status(400).send(error)
+        }
+    })
+
+router.route(["/playlists/:id/tracks"]).get(async (req, res) => {
+    try {
+        const playlist_id = req.params.id;
+        const response = await getTracksByPlaylistId(playlist_id)
+
+        if (response.length === 0) {
+            return res.status(400).json({message: "no tracks in this playlists"})
+        }
+        res.status(200).send(response)
+    } catch (error) {
+        res.status(400).send(error)
+    }
+}).post(async (req, res) => {
+        try {
+            const playlist_id = req.params.id;
+            const { track_id } = req.body
+            const response = await createPlaylist_Track(playlist_id, track_id)
+            res.status(200).send(response)
+        } catch (error) {
+            res.status(400).send(error)
+        }
     })
--- db/query.js
+++ db/query.js
@@ -21,16 +21,20 @@ export async function createPlaylist_Track({ playlist_id, track_id }) {
         INSERT INTO playlists_tracks (playlist_id, track_id)
         VALUES($1, $2) RETURNING *
         `;
-    const { rows: playlists_tracks } = await db.query(
-      SQL, [playlist_id, track_id]
-    );
+    const { rows: playlists_tracks } = await db.query(SQL, [
+      playlist_id,
+      track_id,
+    ]);
     return playlists_tracks;
   } catch (error) {
     console.error(error);
   }
 }
 
-export async function createPlaylist({ playlists_name, playlists_description }) {
+export async function createPlaylist({
+  playlists_name,
+  playlists_description,
+}) {
   try {
     const SQL = `
         INSERT INTO playlists (playlists_name, playlists_description)
@@ -56,6 +60,72 @@ export async function createTrack({ track_name, track_duration }) {
     const { rows: track } = await db.query(SQL, [track_name, track_duration]);
     console.log(track);
     return track;
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+export async function getTracks() {
+  try {
+    const SQL = `
+        SELECT * from tracks
+        `;
+    const { rows: tracks } = await db.query(SQL);
+    console.log(tracks);
+    return tracks;
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+export async function getPlaylists() {
+  try {
+    const SQL = `
+        SELECT * from playlists
+        `;
+    const { rows: playlists } = await db.query(SQL);
+    console.log(playlists);
+    return playlists;
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+export async function getTracksId() {
+  try {
+    const SQL = `
+        SELECT * from tracks WHERE id = $1
+        `;
+    const { rows: tracksId } = await db.query(SQL, [id]);
+    console.log(tracksId);
+    return tracksId;
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+export async function getPlaylistsId() {
+  try {
+    const SQL = `
+        SELECT * from playlists WHERE id = $1
+        `;
+    const { rows: playlistsId } = await db.query(SQL, [id]);
+    console.log(playlistsId);
+    return playlistsId;
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+export async function getTracksByPlaylistId(playlist_id) {
+  try {
+    const SQL = `
+    SELECT tracks.id, tracks.track_name, tracks.track_duration from tracks
+    JOIN playlists_tracks ON tracks.id = playlists_tracks.track_id
+    WHERE playlist_tracks.playlist_id = $1
+    `;
+    const { rows: tracksInPlaylists } = await db.query(SQL, [playlist_id]);
+    return tracksInPlaylists;
   } catch (error) {
     console.error(error);
   }
